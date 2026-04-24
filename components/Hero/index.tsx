5390d4847108398e2ec623a86c9816e15b60c5f3
Your Name <your.email@example.com>
2026-04-24 12:20:21 +0530

    Add GitHub URL and update Get Started link

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -25,13 +25,13 @@ const Hero = () => {
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="https://nandish-tech.com"
+                    href="https://start-up-website-template-veqd.vercel.app/"
                     className="rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
                     Get Started
                   </Link>
                   <Link
-                    href="https://github.com/nandish-tech/template"
+                    href="https://github.com/nandishtech1"
                     className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                   >
                     View on GitHub
